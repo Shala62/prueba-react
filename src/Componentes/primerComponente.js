d5ef4c07ca953732ee0bdb054c46e1948ec7a970
Shala62~ <danielsoto200462@gmail.com>
2025-06-12 17:19:28 -0400

    Pusheo inicial prueba-react

---
+++ src/Componentes/primerComponente.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const PrimerComponente = () => {
+
+  let nombre = "Dani Soto";
+  
+  return (
+    <div>
+      <p>Hola soy el primer componente</p>
+      <p>Mi nombre es {nombre}</p>
+    </div>
+  )
+}
