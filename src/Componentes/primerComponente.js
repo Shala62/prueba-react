3248c14e7b3ad038c9b7670c175b81d614f07e37
Shala62~ <danielsoto200462@gmail.com>
2025-06-12 17:43:04 -0400

    Añadidura de cambio de nombre automático

--- src/Componentes/primerComponente.js
+++ src/Componentes/primerComponente.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 export const PrimerComponente = () => {
 
-  let nombre = "Dani Soto";
-  
+  //let nombre = "Dani Soto";
+
+  const [nombre, setNombre] = useState("Dani Soto");
+  const cambiarNombre = (nuevoNombre) => {
+    setNombre(nuevoNombre);
+  }
+
   return (
     <div>
       <p>Hola soy el primer componente</p>
       <p>Mi nombre es {nombre}</p>
+      <button onClick={ e => cambiarNombre("Daniel Soto Aniñir")}>
+        Cambiar Nombre
+      </button> <br></br>
+
+      <input type="text" onChange={e => cambiarNombre(e.target.value)} placeholder='Cambia el Nombre'></input>
     </div>
   )
 }
